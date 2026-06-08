e5e8b373dab1ca96422433703becf07122c0a3cd
dlwlqxmlove-alt <dlwlqxmlove@gmail.com>
2026-06-08 17:09:10 +0900

    2606081710_commit

--- js/sub.js
+++ js/sub.js
@@ -546,3 +546,22 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+const cards = document.querySelectorAll('.notice-card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
+
